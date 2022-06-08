a5e9b6a3243b6757f56a00da75a1409bb7ae3580
Mariquisha <mariquishamatthews@gmail.com>
2022-06-07 22:23:59 -0400

    added characters

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const characterData = require('./onepiececharacters.json')
+
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.send("One Piece Characters")
+})
+
+app.get('/characters', (req, res) => {
+    res.send(characterData)
+})
+
+app.listen(port, () => {
+    console.log(`App is listening to port ${port}`)
+})
